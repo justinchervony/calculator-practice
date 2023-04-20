632c417afa859aadb68eb96715aaf7c7ebbdbc2f
unknown <justinchervony@gmail.com>
2023-04-20 10:23:12 -0400

    Values and debug created

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello World")
+firstValue = input("Welcome to calculator! Please input the first value: ")
+signValue = input("Now input the process you want to execute. You can select +, -, *, or / : ")
+secondValue = input("Finally, input the last value: ")
+
+print(firstValue, signValue, secondValue)
