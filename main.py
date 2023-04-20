86d4c24838058a2777f78a3531aff4ec6dc5b95f
unknown <justinchervony@gmail.com>
2023-04-20 10:39:01 -0400

    case statements added along with variable type conversion

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-firstValue = input("Welcome to calculator! Please input the first value: ")
-signValue = input("Now input the process you want to execute. You can select +, -, *, or / : ")
-secondValue = input("Finally, input the last value: ")
+firstValue = ''
 
-print(firstValue, signValue, secondValue)
+while firstValue != "quit":
+    firstValue = input("Welcome to calculator! Please input the first value, or type 'quit' to end: ")
+    signValue = input("Now input the process you want to execute. You can select +, -, *, or / : ")
+    secondValue = input("Finally, input the last value: ")
+
+    firstValue = int(firstValue)
+    secondValue = int(secondValue)
+
+    match signValue:
+        case "+":
+            print (firstValue + secondValue)
+        case "-":
+            print (firstValue - secondValue)
+        case "*":
+            print (firstValue * secondValue)
+        case "/":
+            firstValue = float(firstValue)
+            secondValue = float(secondValue)
+            print (firstValue / secondValue)
+
+
+
+
